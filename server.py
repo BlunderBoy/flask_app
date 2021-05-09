64238508e667083abe8a8e6c3c902a5f476c4e3c
razvan.macarie <razvan.macarie@stud.acs.upb.ro>
2021-05-10 02:21:55 +0300

    Added a lot of querries.

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from flask_mysqldb import MySQL
+from random import choice, randrange, random
 import yaml
 
 app = Flask(__name__)
@@ -38,8 +39,347 @@ def index():
 
 		if "Adauga profesori" in request.form:
 			return redirect("/adaugare_profesor_manual")
+		
+		#######
+		if "Studenti cu bursa" in request.form:
+			return redirect("/studenti_bursa")
+
+		if "Studenti corigenti" in request.form:
+			return redirect("/studenti_corigenti")
+
+		if "Clasa dirigentelui" in request.form:
+			return redirect("/clasa_dirigintelui")
+		
+		if "Sortare profesori grad" in request.form:
+			return redirect("/sortare_profesori_grad")
+
+		if "Media clasei" in request.form:
+			return redirect("/media_clasei")
+
+		if "Numar elevi" in request.form:
+			return redirect("/numar_elevi")
 
 	return render_template("index.html")
+			
+@app.route("/studenti_bursa")
+def studenti_bursa():
+	cursor = get_cursor()
+	
+	rezultate = cursor.execute("select nume, prenume, id_clasa, medie from studenti \
+		where medie >= 8.5 order by medie desc")
+
+	if rezultate > 0:
+		studenti = cursor.fetchall()
+		return render_template("studenti_bursa.html", studenti=studenti)
+	else:
+		return "Nu sunt studenti in baza de date"
+			
+@app.route("/studenti_corigenti")
+def studenti_corigenti():
+	cursor = get_cursor()
+	
+	rezultate = cursor.execute("select nume, prenume, id_clasa, medie from studenti \
+		where medie < 5 order by medie desc")
+
+	if rezultate > 0:
+		studenti = cursor.fetchall()
+		return render_template("studenti_corigenti.html", studenti=studenti)
+	else:
+		return "Nu sunt studenti in baza de date"
+			
+@app.route("/clasa_dirigintelui")
+def clasa_dirigintelui():
+	cursor = get_cursor()
+	
+	rezultate = cursor.execute(""" 
+		select p.prenume, p.nume, p.materie, c.nume 
+		from profesori p
+			inner join clase c
+				on p.id_profesor = c.id_diriginte;
+	""")
+
+	if rezultate > 0:
+		profesori = cursor.fetchall()
+		return render_template("clasa_dirigintelui.html", profesori=profesori)
+	else:
+		return "Nu sunt profi in baza de date"
+					
+@app.route("/sortare_profesori_grad")
+def sortare_profesori_grad():
+	cursor = get_cursor()
+	
+	rezultate = cursor.execute(""" 
+		select prenume, nume, grad
+		from profesori
+		order by grad asc;
+	""")
+
+	if rezultate > 0:
+		profesori = cursor.fetchall()
+		return render_template("sortare_profesori_grad.html", profesori=profesori)
+	else:
+		return "Nu sunt profi in baza de date"
+			
+@app.route("/media_clasei")
+def media_clasei():
+	cursor = get_cursor()
+	
+	rezultate = cursor.execute("""
+		select c.nume,
+			(select round(avg(medie), 2) 
+			from studenti s 
+			where s.id_clasa = c.id_clasa)
+		from clase c
+		order by 2 desc;
+	""")
+
+	if rezultate > 0:
+		clase = cursor.fetchall()
+		return render_template("media_clasei.html", clase=clase)
+	else:
+		return "Nu sunt clase in baza de date"
+			
+@app.route("/numar_elevi")
+def numar_elevi():
+	cursor = get_cursor()
+	
+	rezultate = cursor.execute("""
+		select c.nume,
+		(
+			select count(s.id_clasa) from studenti s
+			where s.id_clasa = c.id_clasa
+		)
+		from clase c
+		order by 2 desc
+	""")
+
+	if rezultate > 0:
+		clase = cursor.fetchall()
+		return render_template("numar_elevi.html", clase=clase)
+	else:
+		return "Nu sunt clase in baza de date"
+
+def get_random_date():
+	day = choice(["0","1","2"]) + choice([str(i) for i in range(1,9)])
+
+	month = choice(["0","1"])
+	if month == "0":
+		month += choice([str(i) for i in range(1,9)])
+	else:
+		month += choice(["1","2"])
+
+	year = "0"+choice([str(i) for i in range(5)])
+
+	return day+"/"+month+"/"+year
+
+def get_random_date_enroll():
+	day = choice(["0","1","2"]) + choice([str(i) for i in range(1,9)])
+
+	month = choice(["0","1"])
+	if month == "0":
+		month += choice([str(i) for i in range(1,9)])
+	else:
+		month += choice(["1","2"])
+
+	year = choice([str(i) for i in range(18, 21)])
+
+	return day+"/"+month+"/"+year
+
+@app.route("/populate_students")
+def populate_students():
+	cursor = get_cursor()
+
+	names = ["Alex", "Marius", "Cristi", "Irina", "Maria", "Alex", "Alexandra",
+	"Lucian", "Silviu", "Razvan", "Adrian", "Luiza", "Andrei", "Diana", "Cosmin", "Mihaela",
+	"Stefan", "Bogdan"]
+
+	fam_names = ["Popescu",
+	"Ionescu",
+	"Popa",
+	"Pop",
+	"Niță",
+	"Nițu",
+	"Constantinescu",
+	"Stan",
+	"Stanciu",
+	"Dumitrescu",
+	"Dima",
+	"Gheorghiu",
+	"Ioniță",
+	"Marin",
+	"Tudor",
+	"Dobre",
+	"Barbu",
+	"Nistor",
+	"Florea",
+	"Frățilă",
+	"Dinu",
+	"Dinescu",
+	"Georgescu",
+	"Stoica",
+	"Diaconu",
+	"Diaconescu",
+	"Mocanu",
+	"Voinea",
+	"Albu",
+	"Petrescu",
+	"Manole",
+	"Cristea",
+	"Toma",
+	"Stănescu",
+	"Pușcașu",
+	"Tomescu",
+	"Sava",
+	"Ciobanu",
+	"Rusu",
+	"Ursu",
+	"Lupu",
+	"Munteanu",
+	"Moldoveanu",
+	"Mureșan",
+	"Andreescu",
+	"Sava",
+	"Mihăilescu",
+	"Iancu",
+	"Teodorescu",
+	"Moisescu",
+	"Călinescu",
+	"Tabacu",
+	"Negoiță",
+	"Ifrim"]
+
+	for i in range(4, 100):
+		random_nume = choice(names)
+		random_familie = choice(fam_names)
+		rand_telephone = "07"+"".join([str(choice([0,1,2,3,4,5,6,7,8,9])) for _ in range(8)])
+		cursor.execute(
+			"insert into studenti(id, nume, prenume, id_clasa, email, nr_telefon, anul_inrolarii, data_nasterii, medie) \
+				 values (\'{}\', \'{}\', \'{}\', \'{}\', \'{}\', \'{}\', \
+					str_to_date(\'{}\', \'%d/%m/%y\'),\
+					str_to_date(\'{}\', \'%d/%m/%y\'), \'{}\');"
+				 .format(i, random_nume, random_familie, randrange(1, 6), 
+				  random_nume+random_familie+str(choice([0,1,2,3,4,5,6,7,8,9]))+choice(["@yahoo.com","@gmail.com"]),
+				  rand_telephone, 
+				  get_random_date(), 
+				  get_random_date_enroll(), 
+				  round(random()*10, 1))
+		)
+	mysql.connection.commit()
+	cursor.close()
+	return redirect("/studenti")
+
+@app.route("/populate_profesori")
+def populate_profesori():
+	cursor = get_cursor()
+	materii = ["romana", "engleza", "franceza", "mate", "fizica", "chimie", "biologie", "cultura civica", "istorie", "religie", "geografie", "desen", "muzica", "sport", "educatie tehnologica"]
+	names = ["Alex", "Marius", "Cristi", "Irina", "Maria", "Alex", "Alexandra",
+	"Lucian", "Silviu", "Razvan", "Adrian", "Luiza", "Andrei", "Diana", "Cosmin", "Mihaela",
+	"Stefan", "Bogdan"]
+
+	fam_names = ["Popescu",
+	"Ionescu",
+	"Popa",
+	"Pop",
+	"Niță",
+	"Nițu",
+	"Constantinescu",
+	"Stan",
+	"Stanciu",
+	"Dumitrescu",
+	"Dima",
+	"Gheorghiu",
+	"Ioniță",
+	"Marin",
+	"Tudor",
+	"Dobre",
+	"Barbu",
+	"Nistor",
+	"Florea",
+	"Frățilă",
+	"Dinu",
+	"Dinescu",
+	"Georgescu",
+	"Stoica",
+	"Diaconu",
+	"Diaconescu",
+	"Mocanu",
+	"Voinea",
+	"Albu",
+	"Petrescu",
+	"Manole",
+	"Cristea",
+	"Toma",
+	"Stănescu",
+	"Pușcașu",
+	"Tomescu",
+	"Sava",
+	"Ciobanu",
+	"Rusu",
+	"Ursu",
+	"Lupu",
+	"Munteanu",
+	"Moldoveanu",
+	"Mureșan",
+	"Andreescu",
+	"Sava",
+	"Mihăilescu",
+	"Iancu",
+	"Teodorescu",
+	"Moisescu",
+	"Călinescu",
+	"Tabacu",
+	"Negoiță",
+	"Ifrim"]
+	for i in range(1, 20):
+		cursor.execute(
+		"insert into profesori(id_profesor, materie, nume, prenume, salariu, anul_inrolarii, grad) \
+			 values (\'{}\', \'{}\', \'{}\', \'{}\', \'{}\',\
+				str_to_date(\'{}\', \'%d/%m/%y\'), \'{}\');"
+			 .format(i, choice(materii).capitalize(), 
+			 choice(names), 
+			 choice(fam_names),
+			 randrange(4000, 9000, 1000), 
+			 get_random_date_enroll(), 
+			 randrange(1,3))
+	)
+
+	mysql.connection.commit()
+	cursor.close()
+
+	return redirect("/profesori")
+
+@app.route("/populate_clase")
+def populate_clase():
+	cursor = get_cursor()
+
+	cursor.execute(
+		"insert into clase(id_clasa, nume, id_diriginte, profil) values (\'{}\', \'{}\', \'{}\', \'{}\');"
+			 .format(1, "MI1", randrange(1, 19), "Real")
+	)
+
+	cursor.execute(
+		"insert into clase(id_clasa, nume, id_diriginte, profil) values (\'{}\', \'{}\', \'{}\', \'{}\');"
+			 .format(2, "MI2", randrange(1, 19), "Real")
+	)
+
+	cursor.execute(
+		"insert into clase(id_clasa, nume, id_diriginte, profil) values (\'{}\', \'{}\', \'{}\', \'{}\');"
+			 .format(3, "SN1", randrange(1, 19), "Real")
+	)
+
+	cursor.execute(
+		"insert into clase(id_clasa, nume, id_diriginte, profil) values (\'{}\', \'{}\', \'{}\', \'{}\');"
+			 .format(4, "SN2", randrange(1, 19), "Real")
+	)
+
+	cursor.execute(
+		"insert into clase(id_clasa, nume, id_diriginte, profil) values (\'{}\', \'{}\', \'{}\', \'{}\');"
+			 .format(5, "SN3", randrange(1, 19), "Real")
+	)
+
+	mysql.connection.commit()
+	cursor.close()
+	return redirect("/clase")
 
 @app.route("/studenti")
 def studenti():
